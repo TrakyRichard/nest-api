c7a9db996e6617051e3a097eed1c1d0a9484e14c
Richard Traky <richard@Richards-Mac.localdomain>
2021-06-08 19:43:40 +0000

    adding put and delete routs

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Req, Res, } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Req, Res, } from '@nestjs/common';
 import { CreateEventDto } from './dto/create-events-dto';
 import { Request, Response } from "express";
 
@@ -10,8 +10,24 @@ export class EventsController {
         return res.status(200).json({message: "All retrived"});
     }
 
+    @Get('/:eventId')
+    findEvent(@Param('eventId') eventId): String {
+        return `eventId ${eventId}`;
+    }
+
     @Post()
     createEvent(@Body() eventCreated: CreateEventDto): string {
         return `Event created with title:${eventCreated.title}`;
+    }
+
+    @Put('/:eventId')
+    putEvent(@Body() updateEventDto: CreateEventDto, @Param('eventId') eventId): string
+    {
+        return `Event ${eventId} updated with title ${updateEventDto.title}`;
+    }
+
+    @Delete("/:eventId")
+    deleteEvent(@Param('eventId') eventId) : string {
+        return `event ${eventId} deleted`;
     }
 }
